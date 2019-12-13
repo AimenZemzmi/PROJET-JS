5a8af948cead04399af25eca124275b266e507e4
Aimen Zemzmi <aimen.zemzmi@omendo.com>
2019-12-13 16:17:38 +0100

    wip

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -28,66 +28,22 @@ exports.list_user_group = (req, res) => {
     });
 }
 
-exports.list_user_santa_group = (req, res) => {
-    User.find({idGroup : req.params.id}, function (error,adventure) {
-        if(error){
-            res.status(500);
-            res.json({message: "Server error List User Group"});
-        }else {
-            res.status(200);
-            var SantaChild = [];
-            var tableSanta = [];
-            var tableChild = [];
-            adventure.forEach(function(element){
-              tableSanta.push(element["_id"]);
-              tableChild.push(element["_id"]);   
-            });
-
-            var i = 0;
-
-            tableSanta.forEach(function(element){
-              var santa = tableSanta[i];
-              do{
-                var child = tableChild[Math.floor(Math.random()*tableChild.length)];
-              }while(santa == child || child == null);
-
-              SantaChild.push([santa,child]);
-              delete tableChild[tableChild.indexOf(child)];
-              i++;
-            });
-
-            SantaChild.forEach(function(SC){
-              console.log(SC[1],'coucou');
-              User.findOneAndUpdate( {_id: SC[0]} , {idUser: SC[1] }, {new: true}, (error, adventure) => {
-                  if(error){
-                      console.log('erreur');
-                  }
-              });
-            });
-             
-            res.json(SantaChild);
-        }
-    });
-}
-
 exports.create_a_user = (req, res) => {
+    req.body.idUser = 0;
+    req.body.idGroup = req.params.id;
+    let new_user = new User(req.body);
 
-  req.body.idGroup = req.params.id;
-  req.body.idUser = 0;
-
-  let new_user = new User(req.body);
-
-  new_user.save((error, post) => {
-    if(error){
-      res.status(500);
-      console.log(error);
-      res.json({message: "Erreur serveur Create."});
-    }
-    else {
-      res.status(200);
-      res.json(post);
-    }
-  })
+    new_user.save((error, post) => {
+        if(error){
+          res.status(500);
+          console.log(error);
+          res.json({message: "Erreur serveur Create."});
+        }
+        else {
+          res.status(200);
+          res.json(post);
+        }
+    })
 }
 
 exports.get_a_user = (req, res) => {
@@ -115,15 +71,56 @@ exports.update_a_user = (req, res) => {
 }
 
 exports.delete_a_user = (req, res) => {
-  User.remove({_id: req.params.idUser}, (error) => {
-    if(error){
-      res.status(500);
-      console.log(error);
-      res.json({message: "Erreur serveur."});
-    }
-    else {
-      res.status(200);
-      res.json({message: "Article supprimé"});
-    }
-  })
+    User.remove({_id: req.params.idUser}, (error) => {
+        if (error) {
+            res.status(500);
+            console.log(error);
+            res.json({message: "Erreur serveur."});
+        } else {
+            res.status(200);
+            res.json({message: "Article supprimé"});
+        }
+    })
+}
+
+
+exports.list_user_santa_group = (req, res) => {
+    User.find({idGroup : req.params.idGroup}, function (error,adventure) {
+        if(error){
+            res.status(500);
+            res.json({message: "Server error List User Group"});
+        }else {
+            res.status(200);
+            var SantaChild = [];
+            var tableSanta = [];
+            var tableChild = [];
+            adventure.forEach(function(element){
+                tableSanta.push(element["_id"]);
+                tableChild.push(element["_id"]);
+            });
+
+            var i = 0;
+
+            tableSanta.forEach(function(element){
+                var santa = tableSanta[i];
+                do{
+                    var child = tableChild[Math.floor(Math.random()*tableChild.length)];
+                }while(santa == child || child == null);
+
+                SantaChild.push([santa,child]);
+                delete tableChild[tableChild.indexOf(child)];
+                i++;
+            });
+
+            SantaChild.forEach(function(SC){
+                console.log(SC[1],'coucou');
+                User.findOneAndUpdate( {_id: SC[0]} , {idUser: SC[1] }, {new: true}, (error, adventure) => {
+                    if(error){
+                        console.log('erreur');
+                    }
+                });
+            });
+            res.json(SantaChild);
+        }
+    });
 }
--- src/api/routes/userRoute.js
+++ src/api/routes/userRoute.js
@@ -2,29 +2,17 @@ module.exports = (app) => {
   const userController = require('../controllers/userController');
 
 app.route('/user')
-	.get(userController.list_user);
+		.get(userController.list_user);
 
-app.route('/groupForUser/:id')
-	.post(userController.create_a_user)
-	.get(userController.list_user_group)
-  
-app.route('/groupsanta/:id')
-	.get(userController.list_user_santa_group)
+	app.route('/groupForUser/:id')
+		.post(userController.create_a_user)
+		.get(userController.list_user_group);
 
-app.route('/group/:idGroup/user/:idUser')
-	.get(userController.get_a_user)
-	.put(userController.update_a_user)
-	.delete(userController.delete_a_user)
+	app.route('/groupForUser/:idGroup/user/:idUser')
+		.get(userController.get_a_user)
+		.put(userController.update_a_user)
+		.delete(userController.delete_a_user);
 
-app.route('/groupForUser/:id')
-	.post(userController.create_a_user)
-	.get(userController.list_user_group);
-
-//comment for aimen do not touch this code
-  /*
-  app.route('/groupForUser/:idGroup/user/:idUser')
-	.get(userController.get_a_user)
-	.put(userController.update_a_user)
-	.delete(userController.delete_a_user)
-  */
+	app.route('/groupSanta/:idGroup')
+		.get(userController.list_user_santa_group);
 };
